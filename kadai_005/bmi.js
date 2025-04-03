72b74d749fa03de5983fffa4ff250f0b499611e0
t-sekito <mtna_s@ms3.megaegg.ne.jp>
2025-04-03 19:51:55 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,16 @@
+// 変数の宣言
+let weight;
+let height;
+let bmi;
+
+// 値の代入
+// 体重：68kg
+// 身長：1.7m
+weight = 68;
+height = 1.7;
+
+// 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+bmi = weight/(height*height)
+
+// コンソール出力
+console.log(bmi);
